0c640f1ce34d3d1ec54d4de84c9a315bb71e588e
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-03-24 08:06:22 +0200

    19: change the application's single endpoint handler from returning HTML to returning JSON + introduce an in-memory database

--- backend/tests.py
+++ backend/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 
 
 from backend.vocab_treasury import app
@@ -8,8 +9,25 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-    def test_index(self):
-        rv = self.client.get("/")
-        body_str = rv.get_data()
+    def test_get_users(self):
+        rv = self.client.get("/api/users")
+        body_str = rv.get_data(as_text=True)
+        body = json.loads(body_str)
         self.assertEqual(rv.status_code, 200)
-        self.assertEqual(body_str, b"Hello world!")
+        self.assertEqual(
+            body,
+            {
+                "1": {
+                    "id": "1",
+                    "username": "jd",
+                    "email": "john.doe@gmail.com",
+                    "password": "123",
+                },
+                "2": {
+                    "id": "2",
+                    "username": "ms",
+                    "email": "mary.smith@yahoo.com",
+                    "password": "456",
+                },
+            },
+        )
--- backend/vocab_treasury.py
+++ backend/vocab_treasury.py
@@ -4,9 +4,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Hello world!"
+users_list = [
+    {
+        "id": "1",
+        "username": "jd",
+        "email": "john.doe@gmail.com",
+        "password": "123",
+    },
+    {
+        "id": "2",
+        "username": "ms",
+        "email": "mary.smith@yahoo.com",
+        "password": "456",
+    },
+]
+
+
+users = {u["id"]: u for u in users_list}
+
+
+@app.route("/api/users", methods=["GET"])
+def get_users():
+    return users
 
 
 if __name__ == "__main__":
