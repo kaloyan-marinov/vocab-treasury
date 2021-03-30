6dc66dccfa3b848c73c635e92acba482b8962262
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-03-30 11:20:24 +0300

    20: write missing tests to increase the repo's test coverage

--- backend/vocab_treasury.py
+++ backend/vocab_treasury.py
@@ -52,7 +52,7 @@ class User(db.Model):
         return {"id": self.id, "username": self.username}
 
     def __repr__(self):
-        return f"User({self.username}, {self.email})"
+        return f"User({self.id}, {self.username})"
 
 
 class Example(db.Model):
