b9595521e8f7a4b7cda38ae690bf40fd6d17d7e1
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-06-08 20:35:20 +0300

    21: fix a problem, which the removal of `setupTests.ts` in the next-to-last commit gave rise to

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
@@ -9,6 +10,22 @@ test("renders a 'Hello world!' message", () => {
   /*
   The following statement throws a
   `TypeError: expect(...).toBeInTheDocument is not a function`
+
+  The post and comments on
+  https://stackoverflow.com/questions/56547215/react-testing-library-why-is-tobeinthedocument-not-a-function
+  explain that:
+
+  - the reason for the error is that
+    `toBeInTheDocument` is not part of the React Testing Library
+
+  - the problem can be rectified
+    by adding `import '@testing-library/jest-dom' into this file
+
+  - in fact, the problem had been solved to begin with
+    by the Create React App utility itself,
+    because it had added the above-mentioned import statement into `setupTests.ts`
+    (which means that the "21: remove files and boilerplate code, which ..." commit was
+    what gave rise to this problem in this repository)
   */
-  // expect(divElement).toBeInTheDocument();
+  expect(divElement).toBeInTheDocument();
 });
