179e40044cee0bc63dc591144f7ec7bba5c827a0
Kaloyan Marinov <kallo.marinov@gmail.com>
2023-11-17 08:08:02 +0100

    57: make one of the `<Link>` tags in the `<NavigationBar>` get styled with the `active` CSS class from Bootstrap 5

--- frontend/src/features/NavigationBar.tsx
+++ frontend/src/features/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 import { logOut, selectHasValidToken } from "../store";
@@ -14,21 +14,41 @@ export const NavigationBar = () => {
 
   const dispatch = useDispatch();
 
+  const location = useLocation();
+
   const linksForGuestUser = (
     <React.Fragment>
       <div>
-        <Link to="/" className="btn btn-dark">
+        <Link
+          to="/"
+          className={`btn btn-dark ${location.pathname === "/" && "active"}`}
+        >
           VocabTreasury: Home
         </Link>{" "}
-        <Link to="/about" className="btn btn-dark">
+        <Link
+          to="/about"
+          className={`btn btn-dark ${
+            location.pathname === "/about" && "active"
+          }`}
+        >
           About
         </Link>
       </div>
       <div>
-        <Link to="/login" className="btn btn-dark">
+        <Link
+          to="/login"
+          className={`btn btn-dark ${
+            location.pathname === "/login" && "active"
+          }`}
+        >
           Log in
         </Link>{" "}
-        <Link to="/register" className="btn btn-dark">
+        <Link
+          to="/register"
+          className={`btn btn-dark ${
+            location.pathname === "/register" && "active"
+          }`}
+        >
           Register
         </Link>
       </div>
@@ -38,10 +58,20 @@ export const NavigationBar = () => {
   const linksForLoggedInUser = (
     <React.Fragment>
       <div>
-        <Link to="/own-vocabtreasury" className="btn btn-dark">
+        <Link
+          to="/own-vocabtreasury"
+          className={`btn btn-dark ${
+            location.pathname === "/own-vocabtreasury" && "active"
+          }`}
+        >
           Own VocabTreasury
         </Link>{" "}
-        <Link to="/account" className="btn btn-dark">
+        <Link
+          to="/account"
+          className={`btn btn-dark ${
+            location.pathname === "/account" && "active"
+          }`}
+        >
           Account
         </Link>
       </div>
@@ -60,6 +90,7 @@ export const NavigationBar = () => {
   return (
     <React.Fragment>
       {process.env.NODE_ENV === "development" && "<NavigationBar>"}
+      {process.env.NODE_ENV === "development" && <p>{location.pathname}</p>}
       <nav className="navbar navbar-light bg-light">
         {!hasValidToken ? linksForGuestUser : linksForLoggedInUser}
       </nav>
