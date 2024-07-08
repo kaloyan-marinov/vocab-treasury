f2b0261f2dc75d0620f168d2fb8fa2a723bbba23
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-07-08 07:58:59 +0200

    73: provide concrete values in `download-sample-infrastructure-folder-0.sh`

--- download-sample-infrastructure-folder-0.sh
+++ download-sample-infrastructure-folder-0.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Variables
-GITHUB_USER=<username>
-REPO_NAME=ABC
-COMMIT_SHA=abc
-SUB_FOLDER=path/to/sub-folder
+GITHUB_USER=kaloyan-marinov
+REPO_NAME=msdocs-python-flask-azure-container-apps-3
+COMMIT_SHA=068743411e368a3f981d93e3b17b1a8b6724e94b
+SUB_FOLDER=infrastructure
 
 # Download the ZIP archive of the repository at the specific commit
 curl -L -o repo.zip https://github.com/${GITHUB_USER}/${REPO_NAME}/archive/${COMMIT_SHA}.zip
