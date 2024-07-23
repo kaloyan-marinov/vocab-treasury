e0abfaf6fce183b12ea012ca3df2eefbbc32b78f
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-07-23 06:25:29 +0200

    73: augment `infrastructure-backend/main.tf` with configurations for an `azurerm_service_plan` and an `azurerm_linux_web_app` (but, at present, issuing `terraform apply` and then issuing an HTTP request to `/api/users` causes the logs of the deployed Azure Web App to report the following: ``` 2024-07-23T04:17:23.5358823Z ValueError: failed to find an environment variable called 'MYSQL_HOST' 2024-07-23T04:17:24.4800565Z Failed to apply the database migrations, trying in 5 secs... ``` )

--- infrastructure-backend/main.tf
+++ infrastructure-backend/main.tf
@@ -105,63 +105,67 @@ resource "azurerm_mysql_flexible_database" "m_f_s_d" {
   collation           = "utf8mb4_unicode_ci"
 }
 
-# resource "azurerm_service_plan" "s_p" {
-#   name                = "s-p"
-#   resource_group_name = azurerm_resource_group.rg_vocab_treasury_backend.name
-#   location            = azurerm_resource_group.rg_vocab_treasury_backend.location
-#   os_type             = "Linux"
-#   # sku_name            = "P1v2"
-#   # (
-#   # According to the resource at
-#   # https://learn.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-custom-domain?tabs=root%2Cazurecli :
-#   # [To be able to] map an existing custom DNS name to an Azure App Service,
-#   # ... the web app's App Service plan must be a paid tier and not Free (F1).
-#   # )
-#   # sku_name            = "F1"
-#   sku_name            = "B1"
-# }
+resource "azurerm_service_plan" "s_p" {
+  name                = "s-p"
+  resource_group_name = azurerm_resource_group.rg_vocab_treasury_backend.name
+  location            = azurerm_resource_group.rg_vocab_treasury_backend.location
+  os_type             = "Linux"
+  # sku_name            = "P1v2"
+  # (
+  # According to the resource at
+  # https://learn.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-custom-domain?tabs=root%2Cazurecli :
+  # [To be able to] map an existing custom DNS name to an Azure App Service,
+  # ... the web app's App Service plan must be a paid tier and not Free (F1).
+  # )
+  # sku_name            = "F1"
+  sku_name            = "B1"
 
-# resource "azurerm_linux_web_app" "l_w_a" {
-#   name                = "l-w-a"
-#   resource_group_name = azurerm_resource_group.rg_vocab_treasury_backend.name
-#   location            = azurerm_resource_group.rg_vocab_treasury_backend.location
-#   service_plan_id     = azurerm_service_plan.s_p.id
+  depends_on = [
+    azurerm_mysql_flexible_database.m_f_s_d,
+  ]
+}
 
-#   site_config {
-#     # ( Based on https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_web_app#site_config : )
-#     # `always_on` must be explicitly set to false when using `Free`, `F1`, `D1`, or `Shared` Service Plans.
-#     always_on = false
+resource "azurerm_linux_web_app" "l_w_a" {
+  name                = "l-w-a"
+  resource_group_name = azurerm_resource_group.rg_vocab_treasury_backend.name
+  location            = azurerm_resource_group.rg_vocab_treasury_backend.location
+  service_plan_id     = azurerm_service_plan.s_p.id
 
-#     application_stack {
-#       docker_image_name        = "${var.docker_io_username}/${var.name_of_container_image}:${var.tag_for_container_image}"
-#       docker_registry_url      = "https://docker.io"
-#       # docker_registry_username = var.docker_io_username
-#       # docker_registry_password = var.docker_io_access_token
-#     }
-#   }
+  site_config {
+    # ( Based on https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_web_app#site_config : )
+    # `always_on` must be explicitly set to false when using `Free`, `F1`, `D1`, or `Shared` Service Plans.
+    always_on = false
 
-#   # Despite what the following resources state/claim,
-#   # commenting out the next block
-#   # does _not_ hinder the Azure App Service
-#   # from understanding/guessing what port the (above-specified) container is listening on!
-#   #
-#   # https://duckduckgo.com/?q=how+to+tell+azurerm_linux_web_app+what+port+the+deployed+container+is+listening+on&atb=v413-1&ia=web
-#   #
-#   #   https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet
-#   #     `WEBSITES_PORT`
-#   #     For a custom container, the custom port number on the container for App Service to route requests to.
-#   #     By default, App Service attempts automatic port detection of ports 80 and 8080.
-#   #     This setting isn't injected into the container as an environment variable.
-#   #
-#   #   https://stackoverflow.com/questions/72705694/specify-port-while-deploying-a-docker-container-for-terraform-provider-for-azure
-#   #
-#   #     https://github.com/hashicorp/terraform-provider-azurerm/blob/main/examples/app-service/docker-basic/main.tf
-#   app_settings = {
-#     # For details, you can consult
-#     # https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet#custom-containers
-#     "WEBSITES_PORT" = "5000"
-#   }
-# }
+    application_stack {
+      docker_image_name        = "${var.docker_io_username}/${var.name_of_container_image}:${var.tag_for_container_image}"
+      docker_registry_url      = "https://docker.io"
+      # docker_registry_username = var.docker_io_username
+      # docker_registry_password = var.docker_io_access_token
+    }
+  }
+
+  # Despite what the following resources state/claim,
+  # commenting out the next block
+  # does _not_ hinder the Azure App Service
+  # from understanding/guessing what port the (above-specified) container is listening on!
+  #
+  # https://duckduckgo.com/?q=how+to+tell+azurerm_linux_web_app+what+port+the+deployed+container+is+listening+on&atb=v413-1&ia=web
+  #
+  #   https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet
+  #     `WEBSITES_PORT`
+  #     For a custom container, the custom port number on the container for App Service to route requests to.
+  #     By default, App Service attempts automatic port detection of ports 80 and 8080.
+  #     This setting isn't injected into the container as an environment variable.
+  #
+  #   https://stackoverflow.com/questions/72705694/specify-port-while-deploying-a-docker-container-for-terraform-provider-for-azure
+  #
+  #     https://github.com/hashicorp/terraform-provider-azurerm/blob/main/examples/app-service/docker-basic/main.tf
+  app_settings = {
+    # For details, you can consult
+    # https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet#custom-containers
+    "WEBSITES_PORT" = "5000"
+  }
+}
 
 
 
--- infrastructure-backend/variables.tf
+++ infrastructure-backend/variables.tf
@@ -15,3 +15,27 @@ variable "name_4_mysql_server_database" {
   type        = string
   sensitive   = true
 }
+
+
+
+variable "docker_io_username" {
+  description = "username for docker.io account"
+  type        = string
+  sensitive   = true
+}
+
+# variable "docker_io_access_token" {
+#   description = "access token for docker.io account"
+#   type        = string
+#   sensitive   = true
+# }
+
+variable "name_of_container_image" {
+  description = "name of container image"
+  type        = string
+}
+
+variable "tag_for_container_image" {
+  description = "tag for container image"
+  type        = string
+}
